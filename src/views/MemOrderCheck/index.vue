36b3da75529af51890ac0b3f3a84166412d1292e
Jack <shiungchenyu@gmail.com>
2019-03-08 14:35:17 +0800

    會員 訂單狀態 table rwd 以及頁籤調整

--- src/views/MemOrderCheck/index.vue
+++ src/views/MemOrderCheck/index.vue
@@ -1,6 +1,15 @@
 <script>
+import $ from 'jquery'
 export default {
-  name: 'memOrderCheck'
+  name: 'memOrderCheck',
+  mounted () {
+    // 偵測 bs4 nav-pills 開關
+    $('a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+      // console.log(e.target) // newly activated tab
+      // console.log(e.relatedTarget) // previous active tab
+      // console.log($(e.target).attr('aria-controls'))
+    })
+  }
 }
 </script>
 
