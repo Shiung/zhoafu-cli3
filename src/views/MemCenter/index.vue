6a73d7d0568adef4424f679bfdbab451e6b6e6c7
Jack <shiungchenyu@gmail.com>
2019-03-14 16:10:16 +0800

    fix 會員專區 頁籤 rwd

--- src/views/MemCenter/index.vue
+++ src/views/MemCenter/index.vue
@@ -1,10 +1,43 @@
 <script>
 import { mapGetters } from 'vuex'
+import $ from 'jquery'
 export default {
   name: 'memCenter',
   computed: {
     // vuex
-    ...mapGetters(['tokenVal'])
+    ...mapGetters(['tokenVal', 'menuStatus'])
+  },
+  methods: {
+    rwdscrollMenu () {
+      // // 1. js-------------------------------------------
+      let el = this.$el.querySelector('#rwdScrollUse')
+      this.$nextTick(() => {
+        // 必須等頁面加載玩(否則會抓到前一個元素)
+        let target = el.querySelector('#rwdScrollUse .router-link-exact-active')
+        if (!target) return false
+        let offsetNum =
+          target.offsetLeft -
+          el.offsetLeft -
+          el.offsetWidth / 2 +
+          target.offsetWidth / 2
+        $('#rwdScrollUse').animate({
+          'scrollLeft': offsetNum }, 'slow')
+      })
+
+      // 2. jquery-----------------------------------
+      // let el = $('#rwdScrollUse')
+      // this.$nextTick(() => {
+      //   let target = $('#rwdScrollUse .router-link-exact-active')
+      //   if (target.length === 0) return false
+      //   let offsetNum =
+      //     target.offset().left -
+      //     el.offset().left -
+      //     el.width() / 2 +
+      //     target.width() / 2
+      //   $('#rwdScrollUse').animate({
+      //     'scrollLeft': offsetNum }, 500)
+      // })
+    }
   },
   mounted () {
     if (!this.tokenVal) {
@@ -14,12 +47,17 @@ export default {
       })
       this.$router.replace({ name: 'home' })
     }
+    // rwd
+    this.rwdscrollMenu()
   },
   watch: {
     tokenVal (val) {
       if (!val) {
         this.$router.replace({ name: 'home' })
       }
+    },
+    '$route' (to, from) {
+      this.rwdscrollMenu()
     }
   }
 }
